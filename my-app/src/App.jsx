9c859eee488534eb708d8e8813f01781a9925030
Candid001 <abdulbasitmustapha1@gmail.com>
2025-09-29 11:46:35 +0100

    The general menu page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Ads from './components/Ads'
+import Products from './components/Products'
+import Customer from './components/Customer'
+import Join from './components/Join'
+import Footer from './components/Footer'
+
 
 const App = () => {
+
   return (
     <>
       
+      <Header/>
+      <main>
+        <Hero/>
+        <Ads/>
+        <Products/>
+        <Customer/>
+        <Join/>
+      </main>
+      <Footer/>
     </>
   )
 }
--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -4,5 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    theme: {
+      extend: {
+        fontFamily: {
+          inter: ["Inter", "sans-serif"],
+          serif4: ["Source Serif 4", "serif"],
+        },
+      },
+    },
   plugins: [react(), tailwindcss()],
 })
